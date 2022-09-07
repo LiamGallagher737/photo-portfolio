33e649cf3f9d6e2992754bc118ba029196d4470d
Liam Gallagher <ljpgallagher@xtra.co.nz>
2022-09-07 17:29:23 +1200

    first commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const IMAGES = [
+    { alt: "50 cent pointing", src: "point.jpg", col_start: 1, col_end: 3 },
+    { alt: "50 cent pointing", src: "point.jpg", col_start: 3, col_end: 7 },
+    { alt: "50 cent pointing", src: "point.jpg", col_start: 1, col_end: 3 },
+    { alt: "50 cent pointing", src: "point.jpg", col_start: 1, col_end: 3 },
+];
